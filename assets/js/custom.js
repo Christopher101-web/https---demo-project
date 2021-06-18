872cd4f5c1f47c6209037f8d631ac25d2726d874
Christopher101-web <63649346+Christopher101-web@users.noreply.github.com>
2021-06-18 05:54:50 +0100

    E-Commerce demo project

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+jQuery( document ).ready(function( $ ) {
+
+		$('#carousel').flexslider({
+		    animation: "slide",
+		    controlNav: false,
+		    animationLoop: false,
+		    slideshow: false,
+		    itemWidth: 210,
+		    itemMargin: 5,
+		    asNavFor: '#slider'
+		});
+		 
+		$('#slider').flexslider({
+		    animation: "slide",
+		    controlNav: false,
+		    animationLoop: false,
+		    slideshow: false,
+		    sync: "#carousel"
+		});
+ 
+});
